8039a2ac5abe1d0914a4acbd09c4ff3339dd2fca
Ruturaj Salunkhe <103729113+Ruturajwork@users.noreply.github.com>
2024-02-07 12:09:02 +0530

    Update Footer.js

--- pro/frontend/src/components/Footer.js
+++ pro/frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <Flex as="footer" justifyContent="center" py="5" bgColor="gray.200">
       <Text color="blackAlpha.800" size="3xl">
-        Copyright 2022. Ruturaj Store
+        Copyright 2024. Rst Store
       </Text>
     </Flex>
   );
